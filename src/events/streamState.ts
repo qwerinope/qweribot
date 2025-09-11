aba024b49f7edc0da9268301963ca850b8a7d4c9
qwerinope <me@qwerinope.com>
2025-09-11 16:38:19 +0200

    make everything in streamstate uppercase

--- src/events/streamState.ts
+++ src/events/streamState.ts
@@ -4,10 +4,10 @@ import { eventSub, streamerId } from "main";
 
 eventSub.onStreamOnline(streamerId, async msg => {
   await redis.set('streamIsLive', '1');
-  await sendMessage(`${msg.broadcasterDisplayName} IS LIVE! START DIGGING!`);
+  await sendMessage(`${msg.broadcasterDisplayName.toUpperCase()} IS LIVE! START DIGGING!`);
 });
 
 eventSub.onStreamOffline(streamerId, async msg => {
   await redis.del('streamIsLive');
-  await sendMessage(`${msg.broadcasterDisplayName} IS OFFLINE! NO MORE FREE LOOT!`);
+  await sendMessage(`${msg.broadcasterDisplayName.toUpperCase()} IS OFFLINE! NO MORE FREE LOOT!`);
 });
