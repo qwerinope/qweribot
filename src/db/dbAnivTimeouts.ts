a06b0a1e03eb188af58bbc507bb500b123e9a340
qwerinope <me@qwerinope.com>
2025-09-17 23:22:03 +0200

    simplify anivrecord creation

--- src/db/dbAnivTimeouts.ts
+++ src/db/dbAnivTimeouts.ts
@@ -3,10 +3,10 @@ import User from "user";
 import { anivTimeouts } from "db/schema";
 import { type anivBots } from "lib/handleAnivMessage";
 
-export async function createAnivTimeoutRecord(message: string, anivbot: anivBots, user: User, duration: number) {
+export async function createAnivTimeoutRecord(message: string, anivBot: anivBots, user: User, duration: number) {
   await db.insert(anivTimeouts).values({
     message,
-    anivBot: anivbot,
+    anivBot,
     user: parseInt(user.id),
     duration
   });
